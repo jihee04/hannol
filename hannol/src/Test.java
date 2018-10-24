f766c89b5d335fa0a7794f832677bb95c95e36f3
jihee04 <jihee910311@gmail.com>
2018-10-24 18:40:26 +0900

    최초 커밋

---
+++ hannol/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		double d = 0.5;
+//		Double dd = 0.5;
+		if ((Double)d instanceof Double)
+			System.out.println("ssssss");
+
+		
+		
+		int i = 5;
+		if((Integer)i instanceof Integer) {
+			
+		}
+	}
+
+}
